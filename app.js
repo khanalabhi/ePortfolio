5809ed12ae503b639909dbae9ec684c64ba8c42d
Abhinash Khanal <Abhinash.Khanal@snhu.edu>
2020-11-15 14:54:54 -0500

    Add MongoDB connection

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const MongoClient = require('mongodb').MongoClient;
+
 var app = express();
 
 // view engine setup
@@ -19,23 +21,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+  if (err) {
+    console.error(err);
+  } else {
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+    // register a middleware to attach db connection to the request
+    app.use(function (req, res, next) {
+      req.db = client.db('market');
+      next();
+    });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    app.use('/', indexRouter);
+    app.use('/users', usersRouter);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+      next(createError(404));
+    });
+
+    // error handler
+    app.use(function (err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
+  }
 });
 
 module.exports = app;
